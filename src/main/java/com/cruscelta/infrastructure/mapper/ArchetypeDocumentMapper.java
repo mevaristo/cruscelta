8d82488726ee1b280ae8e3f19ff220bb1c06f6b8
mevaristo <matheus.evaristo@protonmail.com>
2026-03-12 20:38:18 -0300

    :bug: Correct metadata key usage.

--- src/main/java/com/cruscelta/infrastructure/mapper/ArchetypeDocumentMapper.java
+++ src/main/java/com/cruscelta/infrastructure/mapper/ArchetypeDocumentMapper.java
@@ -2,16 +2,24 @@ package com.cruscelta.infrastructure.mapper;
 
 import com.cruscelta.domain.entity.ArchetypeCardDocument;
 import org.springframework.ai.document.Document;
-import org.springframework.ai.document.MetadataMode;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 public class ArchetypeDocumentMapper implements Mapper<ArchetypeCardDocument, Document> {
+    private static final String DEFAULT_CARD_NAME = "Unidentified";
+
     @Override
     public ArchetypeCardDocument toTarget(Document source) {
+        var cardName = Optional
+                .ofNullable(source.getMetadata().get(MetadataKey.CARD_NAME))
+                .map(Object::toString)
+                .orElse(DEFAULT_CARD_NAME);
+
         return ArchetypeCardDocument
                 .builder()
-                .cardName(source.getFormattedContent(MetadataMode.valueOf(MetadataKey.CARD_NAME)))
+                .cardName(cardName)
                 .cardDescription(source.getFormattedContent())
                 .build();
     }
